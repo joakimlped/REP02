e5e38daa12536560d6aff310b2ef4ef8869c6faa
joakimlped <112881196+joakimlped@users.noreply.github.com>
2023-04-27 16:20:20 +0200

    Update tcp_client.go

--- tcp_client.go
+++ tcp_client.go
@@ -27,6 +27,6 @@ func main() {
 	log.Printf("reply from proxy: %s", response)
 	
 	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
-log.Println("Kryptert melding: ", string(kryptertMelding))
-_, err = conn.Write([]byte(string(kryptertMelding)))
+	log.Println("Kryptert melding: ", string(kryptertMelding))
+	_, err = conn.Write([]byte(string(kryptertMelding)))
 }
